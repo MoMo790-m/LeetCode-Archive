9f08e89fdd6697af480d806c7d7ae67af5595c6f
Mohamed Medhat <mohamed06832@gmail.com>
2025-12-03 21:40:17 +0200

    Time: 4 ms (8.46%) | Memory: 18.1 MB (36.59%) - LeetSync

--- 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
+++ 2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.py
@@ -1,5 +1,41 @@
 class Solution:
     def mergeArrays(self, nums1: List[List[int]], nums2: List[List[int]]) -> List[List[int]]:
+        """
+        Two Pointer
+        Single pass through both arrays: O(n + m)
+        Total: O(n + m) -> more faster
+        """
+        
+        i , j = 0,0
+        res = []
+
+        while i < len(nums1) and j < len(nums2):
+            if nums1[i][0] == nums2[j][0]:
+                res.append([nums1[i][0], nums1[i][1] + nums2[j][1]])
+                i+=1
+                j+=1
+            elif nums1[i][0] < nums2[j][0]:
+                res.append(nums1[i])
+                i+=1
+            else:
+                res.append(nums2[j])
+                j+=1
+        
+        res.extend(nums1[i:])
+        res.extend(nums2[j:])
+
+        return res
+
+        
+        
+        """
+        Hash Table approach -> 
+        Building dictionaries: O(n + m)
+        Creating union of sets: O(n + m)
+        Sorting the keys: O(k log k) where k is the number of unique ids
+        Building result: O(k)
+        Total: O(n + m + k log k)
+
         dict1 = {}
         dict2 = {}
 
@@ -10,3 +46,6 @@ class Solution:
             dict2[key] = val
         
         return [[key , dict1.get(key,0) + dict2.get(key,0)] for key in sorted(set(dict1) | set(dict2))]
+
+        """
+        
